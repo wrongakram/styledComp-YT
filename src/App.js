ffece490a2e7bd46291a137fade6484078573f2e
Akram Khalid <akram@akrams-imac.home>
2019-10-21 23:13:10 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,52 +1,8 @@
-import React from 'react';
-import styled from 'styled-components';
-
-//Elements
-import {Button, Searchbar} from './styles/elements.js'
-
-//Icons
-import searchIcon from './svg/search.svg'
-import './App.css'
-
-const Container = styled.div`
-  width: 1280px;
-  min-width: 1280px;
-  margin: 0 auto;
-  .row {
-    width: 100%;
-    display: flex;
-    align-items: center;
-  }
-`
-
-const Subhead = styled.h3`
-  font-size: 24px;
-  font-weight: 600;
-  margin: 160px 0 48px;
-`
-
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">  
-    <Container>
-      <Subhead>Buttons</Subhead>
-        <div className="row">
-          <Button href="/">Playlist</Button>
-          <Button href="/" outline>Playlist</Button>
-          <Button href="/" primary>Songs</Button>
-          <Button href="/" secondary>Songs</Button>
-        </div>
-        <Subhead>Search Bar</Subhead>
-        <div className="row">
-          <Searchbar>
-           <img src={searchIcon} alt="searchIcon"/>
-           <input placeholder="Search for podcasts, news, stories, etc." type="text"/>
-          </Searchbar>
-        </div>
-      </Container>
-    </div>
-  );
+  return <div className="App">hi youtube</div>;
 }
 
 export default App;
